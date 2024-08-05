0564d86e5d3b5198c8c162d667531e8886a9cf9d
AkhmetovAidyn <ahmetov.aidyn1988@mail.ru>
2024-08-05 09:55:13 +0500

    split-string.js

---
+++ 6kyu/split-string.js
@@ -0,0 +1,31 @@
+// ❓ Description:
+
+
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+//     Examples:
+//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+// ✔️ SOLUTION:
+
+function solution(str) {
+
+    if (str.length % 2 !== 0) {
+        str += '_';
+    }
+
+
+    const pairs = [];
+
+
+    for (let i = 0; i < str.length; i += 2) {
+        pairs.push(str.substring(i, i + 2));
+    }
+
+    return pairs;
+}
+
+console.log(solution('abc'));
+console.log(solution('abcdef'));
