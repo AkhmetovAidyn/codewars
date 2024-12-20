e38096d1f7c26aa65425005ce3227e8aec74e3d0
Akhmetov Aidyn <ahmetovv_aidyn@mail.ru>
2024-12-20 10:23:59 +0500

    refactoring quadrants

--- 8kyu/quadrants.ts
+++ 8kyu/quadrants.ts
@@ -12,3 +12,16 @@
 
 
 // ✔️ SOLUTION:
+
+function quadrant(x, y) {
+    // Poveli!
+    if(x > 0 && y > 0) {
+        return 1;
+    } else if(x < 0 && y > 0) {
+        return 2;
+    } else if(x < 0 && y < 0) {
+        return 3;
+    } else {
+        return 4;
+    }
+}
